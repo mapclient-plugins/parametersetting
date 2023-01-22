90c4280a6d46e83be2ec8fab439a33566a53957c
TimmyNic <tim.nicolas@outlook.com>
2023-01-23 11:56:15 +1300

    Set version to 0.1.2

--- mapclientplugins/parametersettingstep/__init__.py
+++ mapclientplugins/parametersettingstep/__init__.py
@@ -2,7 +2,7 @@
 MAP Client Plugin
 '''
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Parameter Setting'
 __location__ = 'https://github.com/mapclient-plugins/parametersetting/archive/v0.1.1.zip'
